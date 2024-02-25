22904c718b09017f655856e7bdb1633849147d29
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-25 17:45:15 +0200

    Alert Confirmation Action

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,6 +2,7 @@
     class="rounded-md bg-pink-600 px-4 py-3 text-sm font-semibold text-white shadow-md hover:bg-pink-500"
     type="submit"
     wire:click="{{ $click }}"
+    wire:confirm="Are you sure you want to add this task?"
     >
     {{ $button }}
 </button>
