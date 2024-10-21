e613dad6b47951f46fcd6f8e50965a7cc0a155e1
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-10-21 17:30:39 +0300

    Validating input

--- app/Livewire/BirdForm.php
+++ app/Livewire/BirdForm.php
@@ -3,15 +3,20 @@
 namespace App\Livewire;
 
 use App\Models\Entry;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class BirdForm extends Component
 {
+    #[Validate('required|integer')]
     public int $birdCount;
+    #[Validate('required|string')]
     public string $notes;
 
     public function submit()
     {
+        $this->validate();
+
         Entry::create([
             'bird_count' => $this->birdCount,
             'notes' => $this->notes,
--- resources/views/livewire/bird-form.blade.php
+++ resources/views/livewire/bird-form.blade.php
@@ -13,6 +13,14 @@
         </div>
     </form>
 
+    @if ($errors->any())
+        <div>
+            @foreach ($errors->all() as $error)
+                <div>{{ $error }}</div>
+            @endforeach
+        </div>
+    @endif
+
     <div>
         @foreach ($entries as $entry)
             <div>
